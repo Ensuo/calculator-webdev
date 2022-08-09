a5c68d40421dba41bd35a990ec867ed1d2009477
Ensuo <haradayasuo2010@gmail.com>
2022-08-09 14:25:15 -0300

    Deleted queue.js, added floats, updated color scheme, added sign change, and cleaned parts of code that were redundant

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const grid = document.querySelector("#buttons-grid");
 let displayValue = '0';
 let firstNumber = '';
 let secondNumber = '';
+let currentSign = '+';
 let currentOperation = null;
-let resetScreen = false;
+let isFloat = false;
 
+//Math operations
 function add(a, b) {
     return a + b;
 }
@@ -17,140 +19,180 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-    return (parseFloat(a * b).toPrecision(12));
+    return a*b;
 }
 
 function divide(a, b) {
-    return (parseFloat(a / b).toPrecision(12));
+    return a/b;
 }
-
+//Does the calculations, depending on the currentOperator
 function calc(operation, a, b) {
     currentOperation = null;
-    int_a = parseInt(a);
-    int_b = parseInt(b);
+    if(isFloat){
+        a_value = parseFloat(a);
+        b_value = parseFloat(b);
+    }else{
+        a_value = parseInt(a);
+        b_value = parseInt(b);
+    }
     switch (operation) {
         case '+':
-            return add(int_a, int_b);
+            return add(a_value, b_value);
             break;
         case '-':
-            return subtract(int_a, int_b);
+            return subtract(a_value, b_value);
             break;
         case '*':
-            return multiply(int_a, int_b);
+            return multiply(a_value, b_value);
             break;
         case '/':
-            return divide(int_a, int_b);
+            return divide(a_value, b_value);
             break;
     }
 }
 
+//Updates the value being displayed
 function updateDisplay(value) {
     text = document.createTextNode(value);
     display.appendChild(text);
 }
 
+//Resets value of display
 function resetDisplay() {
     display.innerHTML = "";
 }
 
+//Resets all variables
 function resetValues(){
     displayValue = '0';
     firstNumber = "";
     secondNumber = "";
+    currentSign = '+';
     currentOperation = null;
-    resetScreen = false;    
+    isFloat = false;    
 }
 
 function inputNumber(value){
-    if(currentOperation == null){
+    if(currentOperation == null){ //If there is no operation, then it is the first number being inputed.
         resetDisplay();
-        if(firstNumber == '0'){
+        if(firstNumber == '0'){ //If it's still 0, then it's the first number being inputed since the start or after a reset.
             firstNumber = value;
-        }else{
+        }else{//After the first digit
             firstNumber += value;
         }
-        displayValue = firstNumber;
-    }else{
+        displayValue = firstNumber; //First number being displayed
+    }else{ //If a math operation was already chosen, then that must mean the first Number has already been chosen, so start creating second number.
         resetDisplay();
         secondNumber += value;
-        displayValue = secondNumber;
+        displayValue = secondNumber; //Second Number now being displayed
     }
 }
 
-function displayAddition(){
+//Math functions that update the value being displayed, result depending on the given operation.
+
+function values_calc(operation){
     if(firstNumber != ''){
         if(secondNumber == ''){
-            currentOperation = '+';
+            if(isFloat){
+                isFloat = false;
+            }
+            currentOperation = operation;
         }else{
             firstNumber = calc(currentOperation, firstNumber, secondNumber);
             displayValue = firstNumber;
             secondNumber = '';
-            currentOperation = '+';
+            currentOperation = operation;
         }
     }
 }
 
-function displaySubtraction(){
-    if(firstNumber != ''){
-        if(secondNumber == ''){
-            currentOperation = '-';
-        }else{
-            firstNumber = calc(currentOperation, firstNumber, secondNumber);
+//Converts current number to float
+function toFloat(){
+    if(secondNumber == ''){
+        if(firstNumber != '' && !isFloat){
+            isFloat = true;
+            firstNumber += '.';
             displayValue = firstNumber;
-            secondNumber = '';
-            currentOperation = '-';
+
+        }
+    }else{
+        if(secondNumber != '' && !isFloat){
+            isFloat = true;
+            secondNumber += '.';
+            displayValue = secondNumber;
         }
     }
 }
 
-function displayMultiplication(){
-    if(firstNumber != ''){
-        if(secondNumber == ''){
-            currentOperation = '*';
+//Change sign of current number being displayed
+function changeSign(){
+    if(displayValue != '0'){
+        if(currentSign == '+'){
+            currentSign = '-';
         }else{
-            firstNumber = calc(currentOperation, firstNumber, secondNumber);
+            currentSign = '+';
+        
+        }
+        if(secondNumber == ''){
+            if(isFloat){
+                firstNumber = parseFloat(firstNumber) * - 1;
+            }else{
+                firstNumber = parseInt(firstNumber) * -1;
+            }
             displayValue = firstNumber;
-            secondNumber = '';
-            currentOperation = '*';
+        }else{
+            if(isFloat){
+                secondNumber = parseFloat(secondNumber) * - 1;
+            }else{
+                secondNumber = parseInt(secondNumber) * -1;
+            }
+            displayValue = secondNumber;
         }
     }
 }
 
-updateDisplay(displayValue);
+updateDisplay(displayValue); //Adds the 0 to display
 
-
+//Adds the functionality to each button
 document.querySelectorAll(".button").forEach(button => {
     button.addEventListener("click", function (e) {
         switch (e.target.classList[1]) { //switch case, depending on the name of the second class
             case 'number':
-
                 inputNumber(e.target.innerHTML);
                 break;
+            case 'sign':
+                changeSign();
+                break;
             case 'plus':
-                displayAddition();
+                values_calc('+');
                 break;
             case 'minus':
-                displaySubtraction()
+                values_calc('-');
                 break;
             case 'multiplication':
-                
+                values_calc('*');
                 break;
             case 'division':
-                currentOperation = '/';
+                values_calc('/');
                 break;
-            case 'equal':
+            case 'equal': //If the equal button is clicked, and all required values are given (first, second number and operation), then the new display value is the result.
                 if(firstNumber != '' && secondNumber != '' && currentOperation != null){
                     firstNumber = calc(currentOperation, firstNumber, secondNumber);
-                    console.log(firstNumber);
+                    if(isFloat){
+                        isFloat = false;
+                    }
                     displayValue = firstNumber;
                     secondNumber = "";
                 }
                 break;
-            case 'reset':
+            case 'dot':
+                toFloat();
+                break;
+            case 'reset': //Resets all variable values, and display to their initial values.
                 resetValues();
                 break;
         }
-        resetDisplay();
+        resetDisplay(); //After each operation, update the values being displayed
         updateDisplay(displayValue)
     });
 });
